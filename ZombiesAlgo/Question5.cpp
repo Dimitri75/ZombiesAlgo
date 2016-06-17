b766e058861d8f7a2bff1879b740bee77835b329
Noel <noel.sb.corp@gmail.com>
2016-06-17 13:34:40 -0400

    Refactoring

--- ZombiesAlgo/Question5.cpp
+++ ZombiesAlgo/Question5.cpp
@@ -1,24 +1,158 @@
 #include "stdafx.h"
 #include "Question5.h"
-#include "Problem.h"
-#include "TSP.h"
+#include <algorithm>
+
+
+static Distance1 distanceMatrix[MAX][MAX];
+static Status statusList[MAX];
+static double oldMatrix[MAX][MAX];
+static double length;
+static int number;
+static Node nodeList[MAX];
 
 void Question5::proceed(){
 
-	Problem problem;
-	problem.read();
-	problem.calcDistances();
+	Question5::read();
+	Question5::calcDistances();
 
-	TSP tsp;
-	tsp.setProblem(problem);
-	tsp.setMatrix();
-	tsp.sortMatrix();
-	for (int i = 1; i < 38; i++){
-		tsp.findNeighbour(i);
+	Question5::setProblem(statusList);
+	Question5::setMatrix();
+	Question5::sortMatrix();
+	for (int i = 1; i < MAX; i++){
+		Question5::findNeighbour(i);
 	}
 
 	cout << endl
 		<< "Le meilleur parcourt :"
 		<< endl;
-	tsp.findNeighbour(11);
+	Question5::findNeighbour(11);
+}
+
+void Question5::read()
+{
+	ifstream fileCity = Utils::getFile("ville.txt");
+	number = 38;
+	for (int i = 0; i < number; i++)
+	{
+		fileCity >> nodeList[i].index >> nodeList[i].xCoord >> nodeList[i].yCoord;
+	}
+
+	cout << "\n";
+}
+
+
+void Question5::calcDistances()
+{
+	for (int i = 0; i < number; i++)
+	{
+		for (int j = 0; j < number; j++)
+
+		{
+			if (i == j)
+			{
+				distanceMatrix[i][j].next = 0;
+				distanceMatrix[i][j].index = nodeList[j].index;
+			}
+			else
+			{
+				double distanceX = (nodeList[i].xCoord - nodeList[j].xCoord);
+				double distanceY = (nodeList[i].yCoord - nodeList[j].yCoord);
+				distanceMatrix[i][j].next = sqrt((distanceX*distanceX) + (distanceY*distanceY));
+				distanceMatrix[i][j].index = nodeList[j].index;
+			}
+		}
+	}
+}
+
+int Question5::getNumber()
+{
+	return number;
+}
+
+void Question5::setProblem(Status statusList[]){
+
+	for (int i = 0; i < MAX; i++){
+		statusList[i].index = i;
+		statusList[i].approched = 0;
+		statusList[i].fullDistance = 0;
+	}
+}
+
+void Question5::setMatrix()
+{
+	for (int i = 0; i < MAX; i++)
+		for (int j = 0; j < MAX; j++)
+			oldMatrix[i][j] = distanceMatrix[i][j].next;
+}
+
+void Question5::sortMatrix()
+{
+
+	for (int i = 0; i < MAX; i++)
+	{
+		sort(distanceMatrix[i], distanceMatrix[i] + getNumber(), [](Distance1 const &a, Distance1 const &b) {
+		return a.next < b.next; });
+
+
+	}
+}
+void Question5::findNeighbour(int city)
+{
+	int cityCount = getNumber();
+	double length = 0;
+	bool visited[MAX];
+	fill_n(visited, cityCount, false);
+	int path[MAX];
+
+	int cityInit = city;
+
+
+
+	bool finished = false;
+	int indexAway = 0;
+	path[indexAway] = city;
+	while (!finished)
+	{
+		finished = true;
+		for (int i = 1; i < cityCount; i++)
+		{
+			if (!visited[distanceMatrix[city - 1][i].index])
+			{
+				finished = false;
+				length += distanceMatrix[city - 1][i].next;
+				visited[city] = true;
+				city = distanceMatrix[city - 1][i].index;
+				indexAway++;
+				path[indexAway] = city;
+				break;
+			}
+		}
+	}
+
+	length += oldMatrix[city - 1][0];
+	indexAway++;
+
+
+
+
+	cout << "\n Cout du chemin d'approvisionnement : " << length << " a partir de la ville " << cityInit << "\n" << endl;
+
+
+	cout << "\n Chemin d'approvisionnement:\n ";
+
+
+	for (int i = 1; i < cityCount; i++)
+	{
+		cout << path[i - 1];
+
+		if (i != cityCount - 1)
+		{
+			cout << " -> ";
+		}
+		else
+		{
+			cout << " -> " << cityInit << endl;
+
+		}
+	}
 }
--- ZombiesAlgo/Question5.h
+++ ZombiesAlgo/Question5.h
@@ -1,7 +1,51 @@
 #pragma once
+#define MAX 38
+
+typedef struct {
+	int index;
+	bool approched;
+	double fullDistance;
+} Status;
+
+struct City {
+	double x;
+	double y;
+};
+
+struct Distance {
+	int index;
+	double distance;
+};
+
+typedef struct
+{
+	int index;
+	double xCoord;
+	double yCoord;
+} Node;
+
+typedef struct
+{
+	int index;
+	double next;
+} Distance1;
+
+
+
 class Question5
 {
 public:
-	static void proceed();
-};
 
+
+	static void read();
+	static void calcDistances();
+	static double ** getDistanceMatrix();
+	static int getNumber();
+	static void proceed();
+	static void setProblem(Status statusList[]);
+	static void setMatrix();
+	static void sortMatrix();
+	static void findNeighbour(int city);
+private:
+	
+};
