60a99c977cd8df67d7e829f88521b72b2f4516b2
Dimitri75 <bdimitri75@gmail.com>
2016-06-17 14:46:55 -0400

    Question 6

--- ZombiesAlgo/Question6.cpp
+++ ZombiesAlgo/Question6.cpp
@@ -1,6 +1,51 @@
 #include "stdafx.h"
 #include "Question6.h"
 
-void Question6::proceed(){
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+void Question6::proceed(){
+	/*
+	Vos services de renseignement ont pu déterminer la source du virus. 
+	En effet, des entités extraterrestres ont pu propager ce virus dans le but de réduire la planète à l’esclavage. 
+	Vous êtes sur le point de découvrir leur laboratoires et usines de production. 
+	Vous souhaitez sécurisez vos échanges d’informations.
+	Il vous est demandé de mettre en place un système qui permettra de sécuriser vos échanges de messages.
+	*/
+
+	// TODO : something
+}
+
+string Question6::vigenereEncryption(string msg, string key)
+{
+	std::transform(msg.begin(), msg.end(), msg.begin(), ::toupper);
+	std::transform(key.begin(), key.end(), key.begin(), ::toupper);
+	unsigned int j = 0;
+	for (int i = 0; i < msg.length(); i++)
+	{
+		if (isalpha(msg[i]))
+		{
+			msg[i] += key[j] - 'A';
+			if (msg[i] > 'Z') msg[i] += -'Z' + 'A' - 1;
+		}
+		j = j + 1 == key.length() ? 0 : j + 1;
+	}
+	return msg;
+}
+
+string Question6::vigenereDecryption(string msg, string key)
+{
+	std::transform(msg.begin(), msg.end(), msg.begin(), ::toupper);
+	std::transform(key.begin(), key.end(), key.begin(), ::toupper);
+	unsigned int j = 0;
+	for (int i = 0; i < msg.length(); i++)
+	{
+		if (isalpha(msg[i]))
+		{
+			msg[i] = msg[i] >= key[j] ?
+				msg[i] - key[j] + 'A' :
+				'A' + ('Z' - key[j] + msg[i] - 'A') + 1;
+		}
+		j = j + 1 == key.length() ? 0 : j + 1;
+	}
+	return msg;
 }
--- ZombiesAlgo/Question6.h
+++ ZombiesAlgo/Question6.h
@@ -3,5 +3,9 @@ class Question6
 {
 public:
 	static void proceed();
+	static string vigenereEncryption(string msg, string key);
+	static string vigenereDecryption(string msg, string key);
+private:
+	static const int alphabet_length = 26;
 };
 
